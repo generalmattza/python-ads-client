b77c3a4bf83749efbc1bea86cbe64956028f2184
Matthew Davidson <admin@davidson.engineering>
2024-09-08 13:42:02 -0700

    removed temporay code used for testing

--- main.py
+++ main.py
@@ -28,36 +28,5 @@ def main():
     logging.info(configs["application"])
 
 
-def test_ads_client():
-    """Test the ADS client class."""
-    import pyads.testserver
-    from src.ads_client import ADSTarget
-    import pyads
-
-    # pyads_testserver = pyads.testserver.AdsTestServer()
-    # handler = pyads.testserver.AdvancedHandler()
-
-    # handler.add_variable(
-    #     pyads.testserver.PLCVariable(
-    #         "var1", bytes(8), ads_type=pyads.constants.ADST_REAL64, symbol_type="LREAL"
-    #     )
-    # )
-    # handler.add_variable(
-    #     pyads.testserver.PLCVariable(
-    #         "var2", bytes(8), ads_type=pyads.constants.ADST_REAL64, symbol_type="LREAL"
-    #     )
-    # )
-    # with pyads_testserver:
-    client = ADSTarget(adsAddress="127.0.0.1.1.1", adsPort=48898)
-    with client.connection:
-        assert client.connection.is_open
-        variables = (("var1", 1), ("var2", 2))
-        client.batch_write_by_name(variables)
-        varNames = ["var1", "var2"]
-        data = client.readVariables(["var1", "var2"])
-        print(data)
-
-
 if __name__ == "__main__":
-    # main()
-    test_ads_client()
+    main()
